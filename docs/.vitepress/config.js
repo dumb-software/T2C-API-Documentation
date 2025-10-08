b9e02336f20f56b120120a2a1c298deb89c9e973
MicheDePain <167225048+MicheDePainn@users.noreply.github.com>
2025-10-08 10:47:06 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     ['meta', { name: 'theme-color', content: '#9c3535' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:site_name', content: 'T2C API Documentation' }],
-    ['meta', { property: 'og:image', content: '/T2C-API-Documentation/apilogo2.png'}],
+    ['meta', { property: 'og:image', content: '/T2C-API-Documentation/favicon.png'}],
     ['meta', { property: 'og:url', content: 'https://dumb-software.github.io/T2C-API-Documentation/' }],
     ['meta', { name: 'google-site-verification', content: 'XVCEArdPRAwzn5EyP6qS89xwwPJjR7AGCMdQODkZKaM' }],
   ],
