26b7d0591baefa71eb526caf4928df9d094782c9
MicheDePain <167225048+MicheDePainn@users.noreply.github.com>
2025-10-08 11:05:04 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,13 +4,18 @@ export default defineConfig({
   title: "T2C API Documentation",
   description: "Documentation non-officielle de l'API T2C",
   base: '/T2C-API-Documentation/',
-  sitemap: { hostname: 'https://dumb-software.github.io/T2C-API-Documentation/' },
+  sitemap: {
+    hostname: 'https://dumb-software.github.io/T2C-API-Documentation/',
+    transformItems(items) {
+      return items.filter((item) => !item.url.includes('migration'))
+    }
+  },
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: 'https://dumb-software.github.io/T2C-API-Documentation/favicon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
     ['meta', { name: 'theme-color', content: '#9c3535' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:site_name', content: 'T2C API Documentation' }],
-    ['meta', { property: 'og:image', content: '/T2C-API-Documentation/favicon.png'}],
+    ['meta', { property: 'og:image', content: 'https://dumb-software.github.io/T2C-API-Documentation/favicon.png'}],
     ['meta', { property: 'og:url', content: 'https://dumb-software.github.io/T2C-API-Documentation/' }],
     ['meta', { name: 'google-site-verification', content: 'XVCEArdPRAwzn5EyP6qS89xwwPJjR7AGCMdQODkZKaM' }],
   ],
